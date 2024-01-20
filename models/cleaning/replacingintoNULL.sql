df0a337ee6b8b8fef326e0197fa42cce746e30a5
Isaacngkh01 <S10243157@connect.np.edu.sg>
2024-01-20 20:53:26 +0800

    testing

---
+++ models/cleaning/replacingintoNULL.sql
@@ -0,0 +1,13 @@
+UPDATE NORTHWIND_DATA.NWT_SCHEMA.raw_customer
+SET 
+
+WHERE customerid like 'NULL'
+   OR companyname like 'NULL'
+   OR contactname like 'NULL'
+   OR contacttitle like 'NULL'
+   OR ADDRESS like 'NULL'
+   OR CITY like 'NULL'
+   OR POSTALCODE like 'NULL'
+   OR COUNTRY like 'NULL'
+   OR PHONE like 'NULL'
+   OR FAX like 'NULL';;

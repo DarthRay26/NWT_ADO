0fa7f7b7d098c69d7009c10ad333a4a7f531c0d6
Reshav Menon <47269621+DarthRay26@users.noreply.github.com>
2024-01-22 14:55:24 +0800

    fima==nalk pircee

--- models/staging/final_price.sql
+++ models/staging/final_price.sql
@@ -1,6 +1,6 @@
-ALTER TABLE NORTHWIND_DATA.NWT_SCHEMA.raw_order_detail
-ADD COLUMN final_price DECIMAL(10, 2);  -- Adjust the data type based on your requirements
+-- ALTER TABLE NORTHWIND_DATA.NWT_SCHEMA.raw_order_detail
+-- ADD COLUMN final_price DECIMAL(10, 2);  -- Adjust the data type based on your requirements
 
 -- Update the newly added column
 UPDATE NORTHWIND_DATA.NWT_SCHEMA.raw_order_detail
-SET final_price = total_price * (1 - COALESCE(discount, 0));
+SET final_price = unitprice - (unitprice * discount);

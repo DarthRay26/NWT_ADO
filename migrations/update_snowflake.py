906edac50109d5890440d1a14cf6565a450d97d9
fate-wq <123043845+fate-wq@users.noreply.github.com>
2024-01-26 21:35:08 +0800

    Update update_snowflake.py

--- migrations/update_snowflake.py
+++ migrations/update_snowflake.py
@@ -2,6 +2,18 @@ import numpy as np
 import pandas as pd
 import snowflake.connector
 import os
+
+# Connect to Snowflake
+con = snowflake.connector.connect(
+    user=os.getenv('SNOWFLAKE_USER'),
+    password=os.getenv('SNOWFLAKE_PASSWORD'),
+    account=os.getenv('SNOWFLAKE_ACCOUNT'),
+    warehouse=os.getenv('SNOWFLAKE_WAREHOUSE'),
+    database=os.getenv('SNOWFLAKE_DATABASE'),
+    schema=os.getenv('SNOWFLAKE_SCHEMA'),
+    role=os.getenv('SNOWFLAKE_ROLE'),
+)
+
 # Download orders_fresh.csv
 df = pd.read_csv('https://raw.githubusercontent.com/just4jc/Northwind-Traders-Dataset/main/order_fresh.csv')
 

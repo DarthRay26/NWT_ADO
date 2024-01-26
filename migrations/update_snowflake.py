7a7dbbb5e94538d7e6b08353688faf38f5f1924f
fate-wq <123043845+fate-wq@users.noreply.github.com>
2024-01-26 21:58:38 +0800

    Update update_snowflake.py

--- migrations/update_snowflake.py
+++ migrations/update_snowflake.py
@@ -1,14 +1,19 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, sessionmaker
 import os
 
 # Create a SQLAlchemy engine for Snowflake
 engine = create_engine(f'snowflake://{os.getenv("SNOWFLAKE_USER")}:{os.getenv("SNOWFLAKE_PASSWORD")}@{os.getenv("SNOWFLAKE_ACCOUNT")}/{os.getenv("SNOWFLAKE_DATABASE")}/{os.getenv("SNOWFLAKE_SCHEMA")}?role={os.getenv("SNOWFLAKE_ROLE")}&warehouse={os.getenv("SNOWFLAKE_WAREHOUSE")}')
 
-# Create a connection
-with engine.connect() as connection:
-    # Download orders_fresh.csv
-    df = pd.read_csv('https://raw.githubusercontent.com/just4jc/Northwind-Traders-Dataset/main/order_fresh.csv')
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    # Write to Snowflake
-    df.to_sql('ORDERS_FRESH', connection, if_exists='replace', index=False)
+# Download orders_fresh.csv
+df = pd.read_csv('https://raw.githubusercontent.com/just4jc/Northwind-Traders-Dataset/main/order_fresh.csv')
+
+# Write to Snowflake
+df.to_sql('ORDERS_FRESH', con=engine, if_exists='replace', index=False)
+
+# Close the session
+session.close()

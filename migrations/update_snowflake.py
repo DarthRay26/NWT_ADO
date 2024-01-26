ad470328225b332051d79fb243d4c765975942a0
fate-wq <123043845+fate-wq@users.noreply.github.com>
2024-01-26 21:26:10 +0800

    Create update_snowflake.py

---
+++ migrations/update_snowflake.py
@@ -0,0 +1,5 @@
+# Download orders_fresh.csv
+df = pd.read_csv('https://raw.githubusercontent.com/just4jc/Northwind-Traders-Dataset/main/order_fresh.csv')
+
+# Write to Snowflake
+df.to_sql('ORDERS_FRESH', con, if_exists='replace', index=False)

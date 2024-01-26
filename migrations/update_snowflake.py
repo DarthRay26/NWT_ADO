e49575e7af9a2d0c8acb0e7a9f2db1d3ceb26f1e
fate-wq <123043845+fate-wq@users.noreply.github.com>
2024-01-26 22:39:22 +0800

    Update update_snowflake.py

--- migrations/update_snowflake.py
+++ migrations/update_snowflake.py
@@ -14,6 +14,32 @@ conn = connect(
     role=os.getenv("SNOWFLAKE_ROLE"),
 )
 
+# Create a cursor object
+cur = conn.cursor()
+
+# SQL command to create the table if it doesn't exist
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS NORTHWIND_DATA.NWT_SCHEMA.ORDERS_FRESH (
+    ORDERID NUMBER(38,0),
+    CUSTOMERID VARCHAR(16777216),
+    EMPLOYEEID NUMBER(38,0),
+    ORDERDATE DATE,
+    REQUIREDDATE DATE,
+    SHIPPEDDATE DATE,
+    SHIPVIA NUMBER(38,0),
+    FREIGHT NUMBER(38,0),
+    SHIPNAME VARCHAR(16777216),
+    SHIPADDRESS VARCHAR(16777216),
+    SHIPCITY VARCHAR(16777216),
+    SHIPREGION VARCHAR(16777216),
+    SHIPPOSTALCODE VARCHAR(16777216),
+    SHIPCOUNTRY VARCHAR(16777216)
+)
+"""
+
+# Execute the SQL command
+cur.execute(create_table_sql)
+
 # Download orders_fresh.csv
 df = pd.read_csv('https://raw.githubusercontent.com/just4jc/Northwind-Traders-Dataset/main/order_fresh.csv')
 

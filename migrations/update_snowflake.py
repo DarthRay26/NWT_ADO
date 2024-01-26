4d0367645c613673b2782a126c64ee5afb767783
fate-wq <123043845+fate-wq@users.noreply.github.com>
2024-01-26 21:47:41 +0800

    Update update_snowflake.py

--- migrations/update_snowflake.py
+++ migrations/update_snowflake.py
@@ -1,21 +1,13 @@
 import numpy as np
 import pandas as pd
-import snowflake.connector
+from sqlalchemy import create_engine
 import os
 
-# Connect to Snowflake
-con = snowflake.connector.connect(
-    user=os.getenv('SNOWFLAKE_USER'),
-    password=os.getenv('SNOWFLAKE_PASSWORD'),
-    account=os.getenv('SNOWFLAKE_ACCOUNT'),
-    warehouse=os.getenv('SNOWFLAKE_WAREHOUSE'),
-    database=os.getenv('SNOWFLAKE_DATABASE'),
-    schema=os.getenv('SNOWFLAKE_SCHEMA'),
-    role=os.getenv('SNOWFLAKE_ROLE'),
-)
+# Create a SQLAlchemy engine for Snowflake
+engine = create_engine(f'snowflake://{os.getenv("SNOWFLAKE_USER")}:{os.getenv("SNOWFLAKE_PASSWORD")}@{os.getenv("SNOWFLAKE_ACCOUNT")}/{os.getenv("SNOWFLAKE_DATABASE")}/{os.getenv("SNOWFLAKE_SCHEMA")}?role={os.getenv("SNOWFLAKE_ROLE")}&warehouse={os.getenv("SNOWFLAKE_WAREHOUSE")}')
 
 # Download orders_fresh.csv
 df = pd.read_csv('https://raw.githubusercontent.com/just4jc/Northwind-Traders-Dataset/main/order_fresh.csv')
 
 # Write to Snowflake
-df.to_sql('ORDERS_FRESH', con, if_exists='replace', index=False)
+df.to_sql('ORDERS_FRESH', engine, if_exists='replace', index=False)

de17c2bb304523207725032f14895583e7d18646
Isaacngkh01 <S10243157@connect.np.edu.sg>
2024-01-26 16:39:41 +0800

    change employee age

--- models/staging/employee_age.sql
+++ models/staging/employee_age.sql
@@ -4,7 +4,7 @@ ADD COLUMN age INT;
 
 -- Update the newly added age column
 UPDATE NORTHWIND_DATA.NWT_SCHEMA.raw_employees
-SET age = EXTRACT(year FROM CURRENT_DATE()) - EXTRACT(year FROM birthdate) - 
+SET age = 1998 - EXTRACT(year FROM birthdate) - 
           CASE 
               WHEN EXTRACT(month FROM CURRENT_DATE()) < EXTRACT(month FROM birthdate) OR 
                    (EXTRACT(month FROM CURRENT_DATE()) = EXTRACT(month FROM birthdate) AND EXTRACT(day FROM CURRENT_DATE()) < EXTRACT(day FROM birthdate)) 

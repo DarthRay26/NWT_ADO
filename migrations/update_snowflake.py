052aa36f78443f869ef53a2d2cad81b2a1bfb11b
fate-wq <123043845+fate-wq@users.noreply.github.com>
2024-01-26 21:54:05 +0800

    Update update_snowflake.py

--- migrations/update_snowflake.py
+++ migrations/update_snowflake.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
 import os
@@ -6,8 +5,10 @@ import os
 # Create a SQLAlchemy engine for Snowflake
 engine = create_engine(f'snowflake://{os.getenv("SNOWFLAKE_USER")}:{os.getenv("SNOWFLAKE_PASSWORD")}@{os.getenv("SNOWFLAKE_ACCOUNT")}/{os.getenv("SNOWFLAKE_DATABASE")}/{os.getenv("SNOWFLAKE_SCHEMA")}?role={os.getenv("SNOWFLAKE_ROLE")}&warehouse={os.getenv("SNOWFLAKE_WAREHOUSE")}')
 
-# Download orders_fresh.csv
-df = pd.read_csv('https://raw.githubusercontent.com/just4jc/Northwind-Traders-Dataset/main/order_fresh.csv')
+# Create a connection
+with engine.connect() as connection:
+    # Download orders_fresh.csv
+    df = pd.read_csv('https://raw.githubusercontent.com/just4jc/Northwind-Traders-Dataset/main/order_fresh.csv')
 
-# Write to Snowflake
-df.to_sql('ORDERS_FRESH', engine, if_exists='replace', index=False)
+    # Write to Snowflake
+    df.to_sql('ORDERS_FRESH', connection, if_exists='replace', index=False)

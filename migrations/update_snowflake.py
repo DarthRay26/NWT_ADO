4be4c5a791744eb41d8264b11e563deb9f56aa52
fate-wq <123043845+fate-wq@users.noreply.github.com>
2024-01-26 21:31:36 +0800

    Update update_snowflake.py

--- migrations/update_snowflake.py
+++ migrations/update_snowflake.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import snowflake.connector
+import os
 # Download orders_fresh.csv
 df = pd.read_csv('https://raw.githubusercontent.com/just4jc/Northwind-Traders-Dataset/main/order_fresh.csv')
 

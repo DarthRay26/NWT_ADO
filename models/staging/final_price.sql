c8ecf743f07866467dbb1a229e824aa90da5bcbd
Reshav Menon <47269621+DarthRay26@users.noreply.github.com>
2024-01-28 00:28:36 +0800

    final price

--- models/staging/final_price.sql
+++ models/staging/final_price.sql
@@ -1,12 +1,10 @@
--- Add total_price column to raw_order_detail
 ALTER TABLE NORTHWIND_DATA.NWT_SCHEMA.raw_order_detail
-ADD COLUMN total_price DECIMAL(10, 2); -- Assuming appropriate data type for total_price
+ADD COLUMN final_price DECIMAL(10, 2); -- Assuming appropriate data type for total_price
 
 -- Update the newly added total_price column
 UPDATE NORTHWIND_DATA.NWT_SCHEMA.raw_order_detail
-SET total_price = unitprice * quantity;
+SET final_price = unitprice * quantity * (1 - discount);
 
 -- Verify the changes
 SELECT *
 FROM NORTHWIND_DATA.NWT_SCHEMA.raw_order_detail;
-

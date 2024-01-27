fb9d078f85857cf114a69e504b50875cfacc3f59
Reshav Menon <47269621+DarthRay26@users.noreply.github.com>
2024-01-27 14:32:36 +0800

    last changes?

--- models/append/append_product_columns.sql
+++ models/append/append_product_columns.sql
@@ -1,4 +1,4 @@
-MERGE INTO {{ ref('product') }} P
+MERGE INTO {{ ref('raw_product') }} P
 USING {{ ref('product_fresh') }} PF ON P.ProductID = PF.ProductID
 WHEN MATCHED THEN
   UPDATE SET P.UNIT_COST = PF.UNITCOST
--- models/cleaning/duplicates_employee.sql
+++ models/cleaning/duplicates_employee.sql
@@ -1,35 +1,26 @@
--- employee_territories_duplicates.sql
+-- Remove duplicates from raw_employee
+DELETE FROM raw_employee
+WHERE (employeeid, ROW_CREATED) IN (
+    SELECT employeeid, MIN(ROW_CREATED) AS min_row_created
+    FROM raw_employee
+    GROUP BY employeeid
+    HAVING COUNT(*) > 1
+);
 
--- Identify duplicate entries in EMPLOYEE_TERRITORIES
-WITH duplicate_entries AS (
-  SELECT EMPLOYEEID, TERRITORYID, COUNT(*) AS duplicate_count
-  FROM {{ ref('raw_employee_territories') }}
-  GROUP BY EMPLOYEEID, TERRITORYID
-  HAVING COUNT(*) > 1
-)
+-- Remove duplicates from raw_employee_territories
+DELETE FROM raw_employee_territories
+WHERE (employeeid, territoryid, ROW_CREATED) IN (
+    SELECT employeeid, territoryid, MIN(ROW_CREATED) AS min_row_created
+    FROM raw_employee_territories
+    GROUP BY employeeid, territoryid
+    HAVING COUNT(*) > 1
+);
 
-
-
--- Find rows with duplicate EMPLOYEEID in EMPLOYEE_TERRITORIES
-WITH duplicate_employeeid AS (
-  SELECT EMPLOYEEID, COUNT(*) AS duplicate_count
-  FROM {{ ref('raw_employee_territories') }}
-  GROUP BY EMPLOYEEID
-  HAVING COUNT(*) > 1
-)
-
-
-
--- Remove duplicate entries in EMPLOYEE_TERRITORIES
-WITH duplicates_to_delete AS (
-  SELECT EMPLOYEEID, TERRITORYID
-  FROM {{ ref('raw_employee_territories') }}
-  GROUP BY EMPLOYEEID, TERRITORYID
-  HAVING COUNT(*) > 1
-)
-
-DELETE FROM {{ ref('raw_employee_territories') }}
-WHERE (EMPLOYEEID, TERRITORYID) IN (
-  SELECT EMPLOYEEID, TERRITORYID
-  FROM duplicates_to_delete
-)
+-- Remove duplicates from raw_order
+DELETE FROM raw_order
+WHERE (orderid, ROW_CREATED) IN (
+    SELECT orderid, MIN(ROW_CREATED) AS min_row_created
+    FROM raw_order
+    GROUP BY orderid
+    HAVING COUNT(*) > 1
+);
